378dd0f985628c684e3e8f2eb44af8012df7ba57
Adrian <adrianjewell91@gmail.com>
2022-09-08 22:14:03 -0400

    Create filter.sh

---
+++ filter.sh
@@ -0,0 +1,2 @@
+curl -H "Content-Type: application/json" -X POST -d '{"username":"mkyong","password":"abc"}' http://localhost:8080/api/login/
+curl -H "Content-Type: application/json" -X POST -d '{"id":"mkyong","title":"abc"}' http://localhost:8080/api/post/
